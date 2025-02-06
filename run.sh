6105c0aa97d15a0efbcac78f1acd064c94e113ee
mr-sarthakgupta <mrsarthakgupta@gmail.com>
2025-02-07 00:36:40 +0530

    update files

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# We have 8 GPUs available, but we need only 4 for the current batch sizes.
+batch_size=( )
+
+for i in {0..3}; do
+  GPU_ID=$((i + 4))  # Shift GPU allocation to 4-7
+  CUDA_VISIBLE_DEVICES=$GPU_ID BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
+done
+
+wait  # Wait for all background processes to finish
+
+echo "All evaluations completed."
