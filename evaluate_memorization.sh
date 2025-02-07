3b364c8b88676746a7844accf3443ba8981819bc
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2025-02-07 09:38:33 +0530

    Update evaluate_memorization.sh

--- evaluate_memorization.sh
+++ evaluate_memorization.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# # Step 1: Run batch_eval.py with the baseline config
-# echo "Running baseline batch evaluation..."
-# python batch_eval.py --model-config configs/models.json --quant-config configs/baseline-config.json --num-samples 10000 --batch-size 128
+# Step 1: Run batch_eval.py with the baseline config
+echo "Running baseline batch evaluation..."
+python batch_eval.py --model-config configs/models_$MODEL_NUM.json --quant-config configs/baseline-config.json --num-samples 10000 --batch-size $BATCH_SIZE
 
-# # Step 2: Merge all JSON files into merged.json
-# echo "Merging all JSON files into merged.json..."
-# find . -name "summary.json" -type f -exec cat {} + | jq -s '.' > ./logs/merged.json
+# Step 2: Merge all JSON files into merged.json
+echo "Merging all JSON files into merged.json..."
+find . -name "summary.json" -type f -exec cat {} + | jq -s '.' > ./logs/merged.json
 
-# echo "Merged JSON files successfully."
+echo "Merged JSON files successfully."
 
 # Step 3: Define swap-every values
 swap_values=(
@@ -23,11 +23,11 @@ swap_values=(
 # Step 4: Run batch_eval.py for each swap-every value
 for swap in "${swap_values[@]}"; do
     echo "Running batch evaluation with swap-every: $swap"
-    python batch_eval.py --model-config configs/models.json \
+    python batch_eval.py --model-config configs/models_$MODEL_NUM.json \
                          --quant-config configs/quant-config.json \
                          --quant-config-swap configs/quant-config.json \
-                         --num-samples 10 \
-                         --batch-size 128 \
+                         --num-samples 10000 \
+                         --batch-size $BATCH_SIZE \
                          --baseline-memorized logs/merged.json \
                          --swap-every "$swap"
 done
