a76e72e811aeecd9e250f41febc0222065c15288
Sarthak Gupta <81774392+mr-sarthakgupta@users.noreply.github.com>
2025-02-10 04:46:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # We have 8 GPUs available, but we need only 4 for the current batch sizes.
-batch_size=(800 400 160 80 32)
+batch_size=(2000 1000 400 200 80)
 
 for i in {0..4}; do
   GPU_ID=$((i))  # Shift GPU allocation to 4-7
